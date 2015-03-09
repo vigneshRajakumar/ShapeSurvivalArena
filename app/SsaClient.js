d9c7b0e874d088005299f85c1934b344384bab5d
vigneshRajakumar <vigneshr.93@gmail.com>
2015-03-09 23:05:35 +0800

    socket connections

--- app/SsaClient.js
+++ app/SsaClient.js
@@ -13,8 +13,43 @@ function SsaClient() {
 	var socket;
 	var playArena;
 
+	var showMessage = function(location, msg) {
+		document.getElementById(location).innerHTML = msg; 
+	}
 
-	var initGUI() {
+    var appendMessage = function(location, msg) {
+        var prev_msgs = document.getElementById(location).innerHTML;
+        document.getElementById(location).innerHTML = "[" + new Date().toString() + "] " + msg + "<br />" + prev_msgs;
+    }
+
+    var sendToServer = function (msg) {
+        var date = new Date();
+        var currentTime = date.getTime();
+        msg["timestamp"] = currentTime;
+        socket.send(JSON.stringify(msg));
+    }    
+
+    var initNetwork = function () {
+    	try {
+    		socket = new SockJS("http://" + Ssa.SERVER_NAME + ":" + Ssa.PORT + "/ssa");
+    		socket.onmessage = function (e) {
+    			var message = JSON.parse(e.data);
+    			switch(message.type) {
+    				case "message":
+    					appendMessage("serverMsg", message.content);
+    					break;
+					default:
+						appendMessage("servermsg", "unhandled message type" +message.type);
+    			}
+    		}
+    	} catch (e) {
+    		console.log("Failed to connect to " + "http://" + Ssa.SERVER_NAME + ":" + Ssa.PORT );
+    	}
+    }
+
+	var initGUI = function() {
+
+		while(document.readyState != "complete") {console.log("lodaing...");};
 
 		playArena = document.getElementById("playArena");
 		playArena.height = Ssa.HEIGHT;
@@ -22,10 +57,11 @@ function SsaClient() {
 	}
 
 	this.start = function() {
+		initNetwork();
 		initGUI();
 	}
 }
 
 //start client
 var gameClient = new SsaClient();
-setTimeout(funtion() {gameClient.start();}, 500);
+setTimeout(function() {gameClient.start();}, 500);
--- app/SsaServer.js
+++ app/SsaServer.js
@@ -15,20 +15,111 @@ function SsaServer() {
 	var port; 	
 	var sockets;
 	var players;
+	var count;
+	var nextPID;
+
+	var broadcast = function(msg) {
+		var id;
+		for(id in sockets) {
+			sockets[id].write(JSON.stringify(msg));
+		}
+	}
+
+	var unicast = function(sock, msg) {
+		sock.write(JSON.stringify(msg));		
+	}
+
+	var reset = function(msg) {
+		//TODO
+	}
+
+	var newPlayer = function(conn) {
+		count++;
+
+		unicast(conn, {type: "message", content:"You are Player " + nextPID})
+
+		//players[conn.id] = new Player()
+		sockets[nextPID] = conn;
+
+		nextPID++;
+	}
+
+	var startGame = function() {
+		//TODO
+	}
 
 	this.start = function() {
 		try {
 			var express = require("express");
 			var http = require("http");
+			var sockjs = require("sockjs");
+			var sock = sockjs.createServer();
+
+			//init
+			count = 0;
+			nextPID = 1;
+			players = new Object();
+			sockets = new Object();
+
+			sock.on("connection", function(conn) {
+				console.log("connected");
+				broadcast({type: "message", content:"There is now "+count+" players"});
+
+				if (count == 4) {
+                    // Send back message that game is full
+                    unicast(conn, {type:"message", content:"The game is full.  Come back later"});
+                    // TODO: force a disconnect
+                } else {
+                    // create a new player
+                    newPlayer(conn);
+                }
+
+				conn.on("close", function () {
+					reset();
+
+					count--;
+
+					nextPID = players[conn.id].pid;
+
+					if (players[conn.id] === p1) p1 = undefined;
+                    if (players[conn.id] === p2) p2 = undefined;
+                    if (players[conn.id] === p3) p3 = undefined;
+                    if (players[conn.id] === p4) p4 = undefined;
+
+					delete players[conn.id];
+
+					broadcast({type:"message", content:" There is now " + count + " players"});
+				});
+
+				conn.on("data", function (data) {
+					var message = JSON.parse(data);
+					var p = players[conn.id];
+
+					if(p==undefined) {
+						return;
+					}
+
+					switch(message.type) {
+						case "start":
+							startGame();
+						default: 
+							console.log("Unhandled message type:" + message.type)
+					}
+				})
+			});
 
 			//start httpServer
 			var app = express();
 			var httpServer = http.createServer(app);
+			sock.installHandlers(httpServer, {prefix:'/ssa'});
 			httpServer.listen(Ssa.PORT, '0.0.0.0');
 			app.use(express.static(__dirname));
+			console.log("Server running on http://0.0.0.0:" + Ssa.PORT + "\n")
+            console.log("Visit http://0.0.0.0:" + Ssa.PORT + "/Ssa.html in your " + 
+                        "browser to start the game")
 		} catch (e) {
 			console.log("Cannot listen to " + port);
-        console.log("Error: " + e);
+        	console.log("Error: " + e);
 		}
 	}
 }
